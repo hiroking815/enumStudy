a3848b820e27978841f0c557c735a483ba625c8c
hiroki <hiroking815@gmail.com>
2018-10-17 10:45:43 +0900

    first

---
+++ src/enumStudy/EnumMain.java
@@ -0,0 +1,10 @@
+package enumStudy;
+
+public class EnumMain {
+
+	public static void main(String[] args) {
+		
+		System.out.println(Shichifukujin.BISHAMONTEN.getIndex());
+		
+	}
+}
---
+++ src/enumStudy/Shichifukujin.java
@@ -0,0 +1,23 @@
+package enumStudy;
+
+public enum Shichifukujin {
+	
+	DAIKOKUTEN(1),
+	BISHAMONTEN(2),
+	EBISUTEN(3),
+	JUROJIN(4),
+	FUKUROKUJU(5),
+	BENZAITEN(6),
+	HOTEIMON(7);
+	
+	private int index;
+	
+	public int getIndex() {
+		return index;
+	}
+
+	Shichifukujin(int index){
+		this.index = index;
+	}
+	
+}
